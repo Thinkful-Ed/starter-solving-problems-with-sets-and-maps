43cbaf16ba32f7dfabcafa23f059debb73efcdc1
Vincent Ramdhanie <vramdhanie@gmail.com>
2021-02-19 15:30:56 -0400

    Add solution for sum pairs problem

--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,22 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+  const numbers = new Map();
+  const solution = new Map();
+  for (let e of A) {
+    numbers.set(e, e);
+  }
+  for (let e of A) {
+    let diff = N - e;
+    if (diff !== e) {
+      if (numbers.has(diff)) {
+        solution.set(Math.min(e, diff), Math.max(e, diff));
+      }
+    }
+  }
+
+  return Array.from(solution);
+}
 
 module.exports = sumPairs;
