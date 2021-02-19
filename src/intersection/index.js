2426cd2eb0dc9613c8b29df3f396bb5c6c5cb4c6
Vincent Ramdhanie <vramdhanie@gmail.com>
2021-02-19 13:09:02 -0400

    Solution for intersection problem

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,17 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+  const map = new Map();
+  const results = new Set();
+  for (let e of a) {
+    map.set(e, e);
+  }
+
+  for (let e of b) {
+    if (map.has(e)) {
+      results.add(e);
+    }
+  }
+
+  return Array.from(results);
+}
 
 module.exports = intersection;
