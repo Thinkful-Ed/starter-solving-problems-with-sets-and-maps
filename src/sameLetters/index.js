2020c18bd26f2c145d6fc120fc84b3705c2f4d27
Vincent Ramdhanie <vramdhanie@gmail.com>
2021-02-19 22:57:48 -0400

    Add solution for sameLetters

--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,19 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+  const results = new Map();
+  words.forEach((word) => {
+    let distinctLetters = Array.from(new Set(word.split("")))
+      .sort()
+      .join("");
+    if (results.has(distinctLetters)) {
+      results.get(distinctLetters).push(word);
+    } else {
+      results.set(distinctLetters, [word]);
+    }
+  });
+  return results;
+}
 
 module.exports = sameLetters;
